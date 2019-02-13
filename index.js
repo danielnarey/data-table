1e9803d6c9c4207e75d6e3fdf3e885cda05ff18c
Daniel C. Narey <daniel@visotype.com>
2019-02-13 15:34:14 -0500

    automated sync

--- index.js
+++ index.js
@@ -4,34 +4,80 @@ const neatCsv = require('neat-csv');
 const stats = require('simple-statistics');
 
 
-const asDataTable = async (jsonArray) => {
+// INTERNAL HELPERS
+
+const whatType = (value) => {
+  if (value === undefined) {
+    return 'Undefined';
+  }
+
+  if (value === null) {
+    return 'Null';
+  }
+
+  return Object.prototype.toString.call(value).slice(8, -1);
+};
+
+
+const isDataTable = async (promise) => {
   let table;
 
   try {
-    const keys = Object.keys(jsonArray[0]);
-
-    const reducer = (a, c, i) => {
-      const obj = a;
-
-      keys.forEach((k) => {
-        obj[k][i] = c[k];
-      });
-
-      return obj;
-    };
-
-    const initial = {};
-    keys.forEach((k) => {
-      initial[k] = [];
-    });
-
-    table = jsonArray.reduce(reducer, initial);
+    table = await promise;
   } catch (error) {
     console.log(error);
   }
 
-  return table;
+  if (whatType(table) !== 'Object') {
+    return false;
+  }
+
+  const keys = Object.keys(table);
+
+  if (keys.length === 0) {
+    return false;
+  }
+
+  const valueTypes = keys.map(k => whatType(table[k]));
+
+  if (!valueTypes.every(x => (x === 'Array'))) {
+    return false;
+  }
+
+  const arrayLengths = keys.map(k => table[k].length);
+
+  if (!arrayLengths.every(x => (x === arrayLengths[0]))) {
+    return false;
+  }
+
+  return true;
 };
+
+
+const fromArray = jsArray => new Promise((resolve, reject) => {
+  if (whatType(jsArray) !== 'Array') {
+    reject(new TypeError(`Expected an Array but got a ${whatType(jsArray)}`));
+  }
+
+  const keys = Object.keys(jsArray[0]);
+
+  const reducer = (a, c, i) => {
+    const obj = a;
+
+    keys.forEach((k) => {
+      obj[k][i] = c[k];
+    });
+
+    return obj;
+  };
+
+  const initial = {};
+  keys.forEach((k) => {
+    initial[k] = [];
+  });
+
+  resolve(jsArray.reduce(reducer, initial));
+});
 
 
 const fromCsv = async (filepath) => {
@@ -39,9 +85,9 @@ const fromCsv = async (filepath) => {
 
   try {
     const csvString = await fs.readFile(filepath);
-    const jsonArray = await neatCsv(csvString);
+    const jsArray = await neatCsv(csvString);
 
-    table = await asDataTable(jsonArray);
+    table = await fromArray(jsArray);
   } catch (error) {
     console.log(error);
   }
@@ -50,13 +96,56 @@ const fromCsv = async (filepath) => {
 };
 
 
+const previewDataFile = async (filepath, bytes = 500, encoding = 'utf8') => {
+  let content;
+
+  try {
+    const fd = fs.openSync(filepath);
+    const { buffer } = await fs.read(fd, Buffer.alloc(bytes), 0, bytes);
+
+    content = buffer.toString(encoding);
+  } catch (error) {
+    console.log(error);
+  }
+
+  return content;
+};
+
+
+const previewRemoteData = (url, bytes = 500, encoding = 'utf8') => new Promise((resolve, reject) => {
+  let content = '';
+  let downloaded = 0;
+
+  try {
+    const stream = got(url, {
+      resolveBodyOnly: true,
+      responseType: 'buffer',
+      stream: true,
+    });
+
+    stream.on('data', (chunk) => {
+      content += chunk.toString(encoding, 0, bytes - downloaded);
+      downloaded += chunk.length;
+      if (downloaded >= bytes) {
+        stream.destroy();
+        resolve(content);
+      }
+    }).on('error', (error) => {
+      reject(error);
+    });
+  } catch (error) {
+    reject(error);
+  }
+});
+
+
 const fromJsonArray = async (filepath) => {
   let table;
 
   try {
-    const jsonArray = await fs.readJson(filepath);
+    const jsArray = await fs.readJson(filepath);
 
-    table = await asDataTable(jsonArray);
+    table = await fromArray(jsArray);
   } catch (error) {
     console.log(error);
   }
@@ -188,6 +277,8 @@ module.exports = {
   fromRemoteCsv,
   fromRemoteJsonArray,
   fromRemoteJsonTable,
+  previewDataFile,
+  previewRemoteData,
   head,
   size,
   sample,
